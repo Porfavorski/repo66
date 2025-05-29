135fb5efcf748ecd85977cffa993e45278b13cf7
Porvaforski <gul4123@proton.me>
2025-05-29 16:57:57 +0000

    Just test code 1.0

---
+++ ifelif.py
@@ -0,0 +1,9 @@
+weith = int(input())
+
+if weith < 60:
+    print('Легкий вес')
+elif 60 <= weith < 64:
+    print('Первый полусредний вес')
+elif 64 <= weith < 69:
+    print('Полусредний вес')
+    
