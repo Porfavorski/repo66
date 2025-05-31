bd633891b72abde4dcbe415cdca69a25efc63e41
Porvaforski <gul4123@proton.me>
2025-05-31 08:38:44 +0000

    Fixerror 1.0

--- newcodessh.py
+++ newcodessh.py
@@ -3,4 +3,13 @@ st = input()
 if '@' in st and '.' in st:
     print("YES")
 else:
+    print("NO")  
+
+
+  
+st = input()
+
+if 'суббота' in st or 'воскресенье' in st:
+    print("YES")
+else:
     print("NO")    
